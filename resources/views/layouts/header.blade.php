c40bb40831188e5ebbed25d8ede46634a5ac884b
thales <thales.ghinis@gmail.com>
2019-04-17 15:46:12 -0300

    alterações no header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,7 +3,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-xs-4">
-                        <p class="site">www.xabalu.pet</p>
+                        <p class="site">www.dogg.pet</p>
                     </div>
                     <div class="col-xs-8 text-right">
                         <p class="num">Telefone: +55-11-5444-1234</p>
@@ -22,23 +22,23 @@
             <div class="container">
                 <div class="row">
                     <div class="col-xs-2">
-                        <div id="colorlib-logo"><a href="{{ url('/home') }}">XABLAU.PET</a></div>
+                        <div id="colorlib-logo"><a href="{{ url('/home') }}">DOGG.pet</a></div>
                     </div>
                     <div class="col-xs-10 text-right menu-1">
                         <ul>
                             <li class="active"><a href="{{ url('/') }}">Home</a></li>
                             <li class="has-dropdown">
-                                <a href="/rooms-suites">Seja um XABLAU.PET</a>
+                                <a href="/rooms-suites">Seja um DOGG.pet</a>
                                 <ul class="dropdown">
                                     <li><a href="#">Parceria</a></li>
                                     <li><a href="#">Quem somos nós</a></li>
-                                    <li><a href="#">Porque ser um XABLAU.PET</a></li>
+                                    <li><a href="#">Porque ser um DOGG.pet</a></li>
                                     <li><a href="#">FAQ</a></li>
                                 </ul>
                             </li>
                             <li><a href="{{ url('/dining-bar') }}">Hotéis &amp; Passeios</a></li>
-                            <li><a href="{{ url('/aminities') }}">Conheça a XABLAU.PET</a></li>
-                            <li><a href="{{ url('/blog') }}">Creche (NOVO)</a></li>
+                            <li><a href="{{ url('/aminities') }}">Conheça a DOGG.pet</a></li>
+                            <li><a href="{{ url('/blog') }}">(A)</a></li>
                             <!-- <li><a href="about.html">Instagram</a></li> -->
                             <li><a href="{{ url('/contact') }}">Contato</a></li>
                             <li><a href="{{ url('/login') }}">Login</a></li>
